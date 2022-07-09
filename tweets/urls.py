371bf3292d3af8e78c299e830e8c8a5ee3eac976
Ravie403 <info@tkm.blue>
2022-07-10 00:28:44 +0900

    set workflow

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+# from django.urls import path
 
-from . import views
+# from . import views
 
-app_name = 'tweets'
+app_name = "tweets"
 urlpatterns = [
     # path('create/', views.TweetCreateView.as_view(), name='create'),
     # path('<int:pk>/', views.TweetDetailView.as_view(), name='detail'),
